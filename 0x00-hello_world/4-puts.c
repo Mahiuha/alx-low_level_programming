8edd747eeb54367eda093ef98946a2ac1c899741
Mahiuha <tiasho.mahiuha@gmail.com>
2021-06-16 15:07:59 -0700

    a simple C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string via puts
+* Description: Program that prints out the string s followed by a new line
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
