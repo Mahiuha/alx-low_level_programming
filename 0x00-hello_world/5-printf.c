bdc94a4d6db996427f7d235f334a62d06c69cd53
Mahiuha <tiasho.mahiuha@gmail.com>
2021-06-16 15:13:18 -0700

    A C program outputing text using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string via printf
+* Description: Program that prints out the string s followed by a new line
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
