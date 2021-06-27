40e7eaacf019cf1750dad12f70007b1bc40ea683
Joseph Mahiuha <35099243+Mahiuha@users.noreply.github.com>
2021-06-27 14:22:03 +0300

    Update 0-isupper.c

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
-* _isupper - returns 1 if uppercase, 0 if not
-* @c: an int given by the main function
-*
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: see first line
-*/
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	return (0);
 }
