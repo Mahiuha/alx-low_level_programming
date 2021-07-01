1a051776c42955468974ee0d3b1770d218829113
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-01 21:33:50 +0300

    some bug fixes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,71 +1,31 @@
 #include "holberton.h"
-#include <limits.h>
-
+#include <stdio.h>
 /**
-* print_number - convers a string to an int
-* @n: a pointer to a string guven by main
-*
-* Description: making our own atoi function
-* Return: it is void
-* to see comments, visit 0x03-more_functions_nested_loops/101-print_number.c
-*/
+ * print_number - that prints an integer
+ * @n: number to string
+ * Returnn: nothing
+ */
 
 void print_number(int n)
 {
-	int result = n;
-	int x;
-	int places = 1000000000;
+	int pot_10 = 1, sign = 1, tmp = n;
 
-	if (n < 0)
+	while (tmp / 10)
 	{
-		result = -n;
+		pot_10 *= 10;
+		tmp /= 10;
+	}
+
+	if (tmp < 0)
+	{
+		sign *= -1;
 		_putchar('-');
 	}
-	if (n == INT_MAX || n == INT_MIN)
+	while (pot_10 > 0)
 	{
-		while (1)
-		{
-			if (n == INT_MAX)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('7');
-				break;
-			}
-			else if (n == INT_MIN)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('8');
-				break;
-			}
-		}
-	}
-	else if (n == 0)
-		_putchar('0');
-	else
-	{
-		while (places > result)
-			places /= 10;
-		while (places > 0)
-		{
-			x = result / places;
-			_putchar((x % 10) + '0');
-			places /= 10;
-		}
+		tmp = n / pot_10;
+		_putchar((tmp * sign) + '0');
+		n = n - (tmp * pot_10);
+		pot_10 /= 10;
 	}
 }
