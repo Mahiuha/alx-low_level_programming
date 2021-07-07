6ab2af0b3fb59acaf8c8af5621aad42792a5a535
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-07 21:39:16 +0300

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,26 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
-* wildcmp - write a function that compares two strings
-* @s1: substring one pointer
-* @s2 :substring two pointer
-*
-* Description: the code returns 1 or 0
-* Return: returns 1 or 0 depending
+* wildcmp - Entry point
+* @s1: char
+* @s2: char
+* Return: Always 0 (Success)
 */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
 		return (0);
-	return (1);
+	if (*s1 == '\0' && *s2 != '*')
+		return (0);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	return (0);
 }
+
