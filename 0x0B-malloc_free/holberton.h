5315e910df7c286a8e798a4f19579c91a0306b8f
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 22:48:13 +0300

    bug fix

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,10 +1,19 @@
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * File: holberton.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-malloc_free directory.
+ */
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-int _putchar(char c);
 char **strtow(char *str);
 
-
+#endif
