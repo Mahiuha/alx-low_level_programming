0ccb59001b61a7867413d4610907e0287ea3a0ce
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 21:00:15 +0300

    bug fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,58 +1,45 @@
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
-* argstostr - a function that concatenates all the arguments
-* @ac: the number of arguments
-* @av: the values of the arguments
-*
-* Description: concatenates the arguments. returns null if any fail
-* Return: the pointer. or null is errors
-* A: if weird cases, return null
-* B: we will coult how big the av 2d array is. go through it and count
-* C: we add up the size of char, times the sum of, total count, ac, and 1
-*	total count cause we want that many, ac for the number of new lines
-*	that we need to print out, and a 1 for the null char
-* D: this part is unneeded since the main uses free
-* E: Start friom the very first argument of ac
-* F: while we do not have a null character in the **av string
-*	we make pointer at k index equal to av index index
-*	we increase k because we need to keep track of our position
-*	we increase J to move along the string until a null char
-*	once we find a null char we will exit loop, assign a new line
-*	increment the k, and go back in the loop until we hit the
-*	very last ac counter.
-* H: after er copied everything fromi the 2d over to the 1d pointer we made
-*	now we will add a null char. and return
-*/
-
+ * argstostr - concatenates all the arguments of my last program
+ *
+ * @ac: number of arguments
+ * @av: double pointer to a string
+ *
+ * Return: a pointer to the new string or Null if it fail
+ */
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int j;
-	int k = 0;
-	int total = 0;
-	char *pointer;
+	int av1 = 0, av2 = 0, size = 0, acc = 0;
+	char *ptr;
 
-	if (ac == 0 || av == NULL)/* A */
-		return (NULL);
-	for (i = 0; i < ac; i++)/* B */
-		for (j = 0; av[i][j]; j++)
-			total++;
-	pointer = (char *) malloc(sizeof(char) * (total + ac + 1));/* C */
-	if (pointer == NULL)/* D */
+	if (ac == 0 || av == NULL)
 	{
-		free(pointer);
 		return (NULL);
 	}
-	for (i = 0; i < ac; i++)/* E */
+	for (av2 = 0; av2 < ac; av2++)
 	{
-		for (j = 0; av[i][j]; j++, k++)/* F */
-			pointer[k] = av[i][j];
-		pointer[k] = '\n';/* G */
-		k++;
+		for (av1 = 0; av[av2][av1] != '\0'; av1++)
+		{
+			size++;
+		}
+		size++;
 	}
-	pointer[k] = '\0';/* H */
-	return (pointer);
+	size++;
+	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (av2 = 0; av2 < ac; av2++)
+	{
+		for (av1 = 0; av[av2][av1] != '\0'; av1++)
+		{
+			ptr[acc] = av[av2][av1];
+			acc++;
+		}
+		ptr[acc] = '\n';
+		acc++;
+	}
+	ptr[acc] = '\0';
+	return (ptr);
 }
