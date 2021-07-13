b792998d0b2983181bc3210a6c1e9c74dc6c640b
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 22:09:25 +0300

    bug fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,44 +1,59 @@
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+int _strlen(char *s);
 /**
- * argstostr - cconcatena dos argumentos
- *
- * @ac: numero de argumentos
- * @av: doble puntero que apunta a una string
- *
- * Return: a pointer
- */
+ * argstostr - concatenate all arguments of your program with newline
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: Null if fail, else return pointer to new string
+ **/
+
 char *argstostr(int ac, char **av)
 {
-	int av1 = 0, av2 = 0, size = 0, acc = 0;
-	char *s;
+	char *s, *t;
+	int x, y, total;
 
 	if (ac == 0 || av == NULL)
+		return (0);
+
+	for (x = 0; x < ac; x++)
 	{
-		return (NULL);
+		for (y = 0; av[x][y] != '\0'; y++, total++)
+			;
+		total++;
 	}
-	for (av2 = 0; av2 < ac; av2++)
-	{
-		for (av1 = 0; av[av2][av1] != '\0'; av1++)
-		{
-			size++;
-		}
-		size++;
-	}
-	size++;
-	s = malloc(size * sizeof(char));
+	total++;
+
+	s = malloc(total * sizeof(char));
+
 	if (s == NULL)
-		return (NULL);
-	for (av2 = 0; av2 < ac; av2++)
+		return (0);
+
+	t = s;
+	for (x = 0; x < ac; x++)
 	{
-		for (av1 = 0; av[av2][av1] != '\0'; av1++)
+		for (y = 0; av[x][y] != '\0'; y++)
 		{
-			s[acc] = av[av2][av1];
-			acc++;
+			*s = av[x][y];
+			s++;
 		}
-		s[acc] = '\n';
-		acc++;
+		*s = '\n';
+		s++;
 	}
-	s[acc] = '\0';
-	return (s);
+	return (t);
 }
+/**
+ * _strlen - return length of a string
+ * @s: char type
+ * Return:  length of string
+ **/
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+
