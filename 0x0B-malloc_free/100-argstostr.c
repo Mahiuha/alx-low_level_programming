9ad74c408af795dc5e2ad87ebef87094c0e39fc0
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 22:40:32 +0300

    bug fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,37 +1,53 @@
 #include <stdlib.h>
+
+int _strlen(char *s);
 /**
-*argstostr - concatenates all the arguments of a program with "\n"
-*@ac: length of av
-*@av: array of strings
-*Return: strings
-*/
+ * argstostr - concatenates all arguments into one string
+ * @ac: int args
+ * @av: array of args
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, size;
-	char *ch;
+	int i;
+	int x;
+	int y = 0;
+	int len = 0;
+	char *array;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		len = len + _strlen(av[i]);
+
+	len = len + 1 + ac;
+
+	array = malloc(sizeof(char) * len);
+
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			size++;
-		size++;
+		for (x = 0; av[i][x] != '\0'; x++, y++)
+			array[y] = av[i][x];
+		array[y] = '\n';
+		x++;
 	}
-	size++;
-	ch = malloc(sizeof(char) * size);
-	if (ch == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			ch[k] = av[i][j];
-			k++;
-		}
-		ch[k] = '\n';
-		k++;
-	}
-	ch[k] = '\0';
-	return (ch);
+	array[y] = '\0';
+	return (array);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The given string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int total;
+
+	for (total = 0; s[total] != '\0'; total++)
+		;
+	return (total);
 }
