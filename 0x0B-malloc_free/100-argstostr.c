46a46ad53c9305f509a6e0fc0a598c1de61664c0
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 23:32:54 +0300

    bug fixes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,52 @@
-
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
+int _strlen(char *ch);
+
 /**
- **argstostr -  a function that concatenates all the arguments of your program.
- *@ac : int
- *@av : array
- *Return: array
+ * argstostr - concatenate arguments.
+ * @ac: arg counter.
+ * @av: arg vector.
+ * Return: pointer p.
  */
 char *argstostr(int ac, char **av)
 {
-	int x, y, i, j, l = 0, a = 0;
-	char *s;
+int i, a = 0, k = 0;
+int j;
+char *p;
+if (ac == 0 || av == NULL)
+return (0);
+for (i = 0; i < ac; i++)
+{
+a += _strlen(av[i]);
+}
+p = malloc(a + 1 + ac);
+if (p == NULL)
+return (0);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; j < (_strlen(av[i])); j++)
+{
+p[k] = av[i][j];
+k++;
+}
+p[k] = '\n';
+k++;
+}
+p[k] = '\0';
+return (p);
+}
 
-	if (ac == 0 || av == NULL)
-	return (NULL);
-	for (i = 0; (i < ac); i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		l++;
-		l++;
-	}
+/**
+ * _strlen - counts number of chars.
+ * @ch: string.
+ * Return: pointer i.
+ */
 
-	s = malloc(sizeof(char) * l + 1);
-	if (s == NULL)
-	return (NULL);
-	for (x = 0; x < ac ; x++)
-	{
-		for (y = 0; av[x][y] != '\0'; y++)
-		{
-			s[a] = av[x][y];
-			a++;
-		}
-		s[a++] = '\n';
-	}
-	s[a] = '\0';
-	return (s);
+int _strlen(char *ch)
+{
+int i = 0;
+while (ch[i])
+i++;
+return (i);
 }
