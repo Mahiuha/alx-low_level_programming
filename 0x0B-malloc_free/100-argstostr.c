e1a91315f6acb26599b2cb2731645bfbb95372fd
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 22:42:26 +0300

    bug fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,53 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char *s);
 /**
- * argstostr - concatenates all arguments into one string
- * @ac: int args
- * @av: array of args
- *
- * Return: a pointer to a new string, or NULL if it fails
+ * *argstostr - convert arguments on command line to strings
+ * @ac: int type
+ * @av: pointer to array
+ * Return: arguments as strings
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int x;
-	int y = 0;
-	int len = 0;
-	char *array;
+	int size, count, count1, count2 = 0;
+	char *ptr;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-		len = len + _strlen(av[i]);
-
-	len = len + 1 + ac;
-
-	array = malloc(sizeof(char) * len);
-
-	for (i = 0; i < ac; i++)
 	{
-		for (x = 0; av[i][x] != '\0'; x++, y++)
-			array[y] = av[i][x];
-		array[y] = '\n';
-		x++;
+		return (NULL);
 	}
-	array[y] = '\0';
-	return (array);
-}
 
-/**
- * _strlen - Returns the length of a string
- * @s: The given string
- *
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-	int total;
+	for (count = 0; count < ac; count++)
+	{
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		{
+			size += 1;
+		}
+		size += 1;
+	}
+	size += 1;
 
-	for (total = 0; s[total] != '\0'; total++)
-		;
-	return (total);
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (count = 0; count < ac; count++)
+	{
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		{
+			ptr[count2] = av[count][count1];
+			count2++;
+		}
+		ptr[count2] = '\n';
+		count2++;
+	}
+	ptr[count2] = '\0';
+	return (ptr);
 }
