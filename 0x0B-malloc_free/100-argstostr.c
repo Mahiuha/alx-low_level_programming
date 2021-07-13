0d4192f2afa66de7826ad17834e4fe4aba50309c
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-13 22:03:20 +0300

    bug fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,62 +1,58 @@
-#include <stdlib.h>
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* argstostr - a function that concatenates all the arguments
-* @ac: the number of arguments
-* @av: the values of the arguments
-*
-* Description: concatenates the arguments. returns null if any fail
-* Return: the pointer. or null is errors
-* A: if weird cases, return null
-* B: we will coult how big the av 2d array is. go through it and count
-* C: we add up the size of char, times the sum of, total count, ac, and 1
-*	total count cause we want that many, ac for the number of new lines
-*	that we need to print out, and a 1 for the null char
-* D: this part is unneeded since the main uses free
-* E: Start friom the very first argument of ac
-* F: while we do not have a null character in the **av string
-*	we make pointer at k index equal to av index index
-*	we increase k because we need to keep track of our position
-*	we increase J to move along the string until a null char
-*	once we find a null char we will exit loop, assign a new line
-*	increment the k, and go back in the loop until we hit the
-*	very last ac counter.
-* H: after er copied everything fromi the 2d over to the 1d pointer we made
-*	now we will add a null char. and return
-*/
-
+ * argstostr - function that concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0
+ */
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int arg, byte, index, size = ac;
+	char *ptr, *beginning;
+	int i, len = 0, j;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (arg = 0; arg < ac; arg++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			size++;
-	}
-
-	str = malloc(sizeof(char) * size + 1);
-
-	if (str == NULL)
 		return (NULL);
-
-	index = 0;
-
-	for (arg = 0; arg < ac; arg++)
-	{
-		for (byte = 0; av[arg][byte]; byte++)
-			str[index++] = av[arg][byte];
-
-		str[index++] = '\n';
 	}
+	for (i = 0; i < ac; i++)
+	{
+		len += 1 + _strlen(av[i]);
+	}
+	ptr = malloc(sizeof(char) * (len + 1));
+	beginning = ptr;
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+			{
+				*ptr++ = av[i][j];
+			}
+			*ptr++ = '\n';
+		}
+		ptr = '\0';
+	}
+	return (beginning);
+}
+/**
+ * _strlen - returns string length
+ * @s: string
+ * Return: strlen
+*/
+int _strlen(char *s)
+{
+	int string;
 
-	str[size] = '\0';
-
-	return (str);
+	for (string = 0; string[s] != '\0'; string++)
+	{
+		;
+	}
+	return (string);
 }
