b2c43669c6a47045a997bae3b623ae2f9718d9fb
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-15 21:43:04 +0300

    bug fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,69 +1,41 @@
-#include "holberton.h"
 #include <stdlib.h>
 
-
-
-
 /**
-  * len - get length of string
-  * @s: str
-  * Return : int
-  */
-
-int len(char *s)
-{
-	int c;
-	for (c = 0; s[c] != '\0'; c++)
-	;
-	return (c);
-}
-
-/**
-  * string_nconcat - concatenates two strings
-  * @s1: string
-  * @s2: string
-  * @n: unsigned int
-  */
+ * string_nconcat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * @n: max chars to get from s2
+ * Return: new string concatenated.
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *array;
-	unsigned int i = 0;
-	unsigned int i_s2;
-	unsigned size_s2 = len(s2);
-	unsigned size = len(s1) + len(s2);
-	if (s1 == NULL)
+	char *r;
+	unsigned int i = 0, j = 0, size = 0;
+
+	if (!s1)
 		s1 = "";
-	if (s2 == NULL)
+
+	if (!s2)
 		s2 = "";
 
-	array = malloc(sizeof(char) * (size + 1));
-	
-	/* check for allocation failure */
-	if (array == NULL)
-		return (NULL);
-	
-	for ( ; *s1 != '\0'; s1++)
-	{
-		array[i] = *s1;
-		i++;
-	}
-	if (n >= size_s2)
-	{
-		for ( ; *s2 != '\0'; s2++)
-		{
-			array[i] = *s2;
-			i++;
-		}
-	}
-	for (i_s2 = 0; i_s2 < n; i_s2++)
-	{
-		if (i_s2 >= size_s2)
-			break;
-		array[i] = *s2;
-		s2++;
-		i++;
-	}
-	array[i] = '\0';
-	return (array);
+	while (s1[i++])
+		size++;
+	while (s2[j++] && j <= n)
+		size++;
+
+	r = malloc(sizeof(char) * (size + 1));
+
+	if (!r)
+		return (0);
+
+	for (i = 0; s1[i]; i++)
+		r[i] = s1[i];
+
+	for (j = 0; j < n && s2[j]; j++)
+		r[j + i] = s2[j];
+
+	r[j + i] = '\0';
+
+	return (r);
 }
