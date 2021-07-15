08422db169f3eae683a51f09d0af88c2a04e696d
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-15 23:08:51 +0300

    more malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,44 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * isnum - checks for a digit.
- * @s: An ascii char to compare
- * Return: Always 0.
- */
-int isnum(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-/**
- * main - multiplies two large numbers
+ * main - int argc, char *argv[]
  * @argc: argument count
  * @argv: argument vector
- * Return: Error or 0 if is correct
+ * description: program that mutiplies two unsigned integers
+ * Return: 0 if successful.
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	(void)argc;
+	(void)argv;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (!isnum(argv[1]) && !isnum(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	printf("%d\n", atoi(argv[2]) * atoi(argv[1]));
+	exit(98);
 	return (0);
 }
-
