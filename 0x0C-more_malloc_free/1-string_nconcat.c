521e7650c2cf8a24356a716a42f5a227f76b4fe6
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-15 21:50:15 +0300

    bug fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,46 @@
-#include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * string_nconcat - concatenates two strings
- * @s1: pointer to first string.
- * @s2: pointer to 2nd string.
- * @n: Number of bytes from n2 to concatenate.
- *
- * Return:Pointer to space in memory containing concatenated string.
+ * string_nconcat - function that concatenates two strings
+ * @s1: input string 1
+ * @s2: input string 2
+ * @n: first n bytes
+ * Return: char
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, j, s1_length, s2_length;
+	char *ptr;
+	unsigned int i = 0;
+	unsigned int i2 = 0;
 
-/*Check if the strings passed are null*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-/*Compute the length of the strings*/
-
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-		;
-
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-		;
-/*Memory reservation-for case 1 & 2.*/
-	str = malloc(s1_length + n + 1);
-	if (str == NULL)
-	{
+	while (s1[i] != '\0')
+		i++;
+	while (s2[i2] != '\0')
+		i2++;
+	i2++;
+	if (n >= i2)
+		n = i2;
+	ptr = malloc(sizeof(*ptr) * n + (i + 1));
+	if (ptr == NULL)
 		return (NULL);
-	}
-/*Copy first string into str.*/
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-/*copy second stringg into str.*/
-	for (j = 0; j < n; j++)
+	i = 0;
+	while (s1[i] != '\0')
 	{
-		str[i] = s2[j];
+		ptr[i] = s1[i];
 		i++;
 	}
-
-	str[i] = '\0';
-	return (str);
+	i2 = 0;
+	while (s2[i2] != '\0' && i2 < n)
+	{
+		ptr[i] = s2[i2];
+		i++;
+		i2++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
