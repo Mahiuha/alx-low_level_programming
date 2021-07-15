e2ec2e1133937d972926a88e6c4f4bbaa0301978
Joseph Mahiuha <35099243+Mahiuha@users.noreply.github.com>
2021-07-15 22:48:43 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,61 +1,52 @@
-#include <stdlib.h>
 #include "holberton.h"
-int _strlen(char *s);
+#include <stdlib.h>
+
 /**
- * string_nconcat - concats two strings
- * @s1: string 1
- * @s2: string 2
- * @n: the number of bytes from s2 to use
- *
- * Return: a pointer for the allocated memory for the new string
- */
+* string_nconcat - Concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+* @n: Unsigned int variable, like a limit.
+*
+* Return: To the value of pointer.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, size1, size2, j;
-	char *cat;
-
+unsigned int i, j, d, f;
+char *e;
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
-		size1 = _strlen(s1);
-		if (n >= (unsigned int) _strlen(s2))
-		size2 = _strlen(s2);
-	else
-		size2 = n;
-
-		cat = malloc(sizeof(char) * (size1 + size2 + 1));
-
-		if (cat == NULL)
-			return (NULL);
-
-	for (i = 0; i < size1; i++)
-	{
-		cat[i] = s1[i];
 	}
-	for (j = 0; j < size2; j++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		cat[i] = s2[j];
-		i++;
-	}
-	cat[i] = '\0';
-
-	return (cat);
-}
-/**
- * _strlen - Return the length of a string
- * @s: The string to check
- *
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
 		;
-
-	return (i);
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		;
+	}
+	if (j > n)
+	{
+		j = n;
+	}
+	e = malloc(sizeof(char) * (i + j + 1));
+	if (e == NULL)
+	{
+		return (NULL);
+	}
+	for (d = 0; s1[d] != '\0'; d++)
+	{
+		e[d] = s1[d];
+	}
+	for (f = 0; f < j; f++)
+	{
+		e[d] = s2[f];
+		d++;
+	}
+	e[i + j] = '\0';
+return (e);
 }
-
