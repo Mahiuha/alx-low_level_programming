8b82c6cbf6f3c40ca105e0f672ae0d59db853742
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-15 22:15:29 +0300

    more malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,75 +1,61 @@
-#include "holberton.h"
 #include <stdlib.h>
-
+#include "holberton.h"
+int _strlen(char *s);
 /**
-* string_nconcat - a function that concats two strings
-* @s1: a string given by main
-* @s2: a string given by main
-* @n: the length given by main.
-*
-* Description: return a pointer that points to new space in memory
-*	which contains s1 and the first n bytes of s2 and a null term
-*	if n is >= s2 then print it all
-* Return: the pointer to the allocated memory
-*/
-
+ * string_nconcat - concats two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: the number of bytes from s2 to use
+ *
+ * Return: a pointer for the allocated memory for the new string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *pointer;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k = 0;
+	int i, size1, size2, j;
+	char *cat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (n >= j)
-		pointer = malloc((i + j + 2) * sizeof(char));
-	if (n < j)
-		pointer = malloc((i + n + 1) * sizeof(char));
-	if (pointer == NULL)
-		return (NULL);
-	if (n >= j)
-	{
-		i = 0;
-		while (s1[i])
-		{
-			pointer[k] = s1[i];
-			i++;
-			k++;
-		}
-		j = 0;
-		while (s2[j])
-		{
-			pointer[k] = s2[j];
-			j++;
-			k++;
-		}
-		pointer[k] = '\0';
-	}
+
+		size1 = _strlen(s1);
+		if (n >= (unsigned int) _strlen(s2))
+		size2 = _strlen(s2);
 	else
+		size2 = n;
+
+		cat = malloc(sizeof(char) * (size1 + size2 + 1));
+
+		if (cat == NULL)
+			return (NULL);
+
+	for (i = 0; i < size1; i++)
 	{
-		i = 0;
-		while (s1[i])
-		{
-			pointer[k] = s1[i];
-			i++;
-			k++;
-		}
-		j = 0;
-		while (n)
-		{
-			pointer[k] = s2[j];
-			n--;
-			j++;
-			k++;
-		}
-		pointer[k] = '\0';
+		cat[i] = s1[i];
 	}
-	return (pointer);
+	for (j = 0; j < size2; j++)
+	{
+		cat[i] = s2[j];
+		i++;
+	}
+	cat[i] = '\0';
+
+	return (cat);
 }
+/**
+ * _strlen - Return the length of a string
+ * @s: The string to check
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
