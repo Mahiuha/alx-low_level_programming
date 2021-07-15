2ee56085be02519cc728e3203957883b33d20686
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-15 21:53:16 +0300

    more malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,50 +2,32 @@
 #include <stdlib.h>
 
 /**
- * _calloc - prints buffer in hexa
- * @nmemb: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
+ * _calloc - Allocates memory for an array of a certain number
+ *           of elements each of an inputted byte size.
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element.
+ *
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the allocated memory.
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	void *mem;
+	char *filler;
+	unsigned int index;
 
-	if (size == 0 || nmemb == 0)
-	{
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	}
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
 		return (NULL);
-	}
-	if (ptr == NULL)
-		return (NULL);
-	_memset(ptr, 0, nmemb * size);
-	return (ptr);
+
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+
+	return (mem);
 }
-#include "holberton.h"
-
-/**
- * *_memset - check the code for Holberton School students.
- * @s: Starting address of memory to be filled.
- * @b: Value to be filled.
- * @n: Number of bytes to be filled starting from ptr to be filled.
- * Return: Char.
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int idx = 0;
-	char *a = s;
-
-	while (idx < n)
-	{
-		a[idx] = b;
-		idx++;
-	}
-	return (a);
-}
-
