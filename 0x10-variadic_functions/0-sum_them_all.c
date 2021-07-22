03d3dff2845b3c3e9979b155d0f795823188ae8c
Mahiuha <tiasho.mahiuha@gmail.com>
2021-07-22 22:17:26 +0300

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,35 +1,24 @@
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - returns the sum of all its parameters
- *
- * @n: the constant unsigned integers that are the number of arguments
- *
- * Return: the sum of all the parameters
- */
+* sum_them_all - adds up all the numbers
+* @n: number of parameters
+*
+* Description: does stuff via variadic functions
+* Return: Returns the sum!
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int runner;
-	int total = 0;
-	/* I created a variable type va_list that iterates the arguments */
-	/* and a variable that runs the arguments */
-	/* has to be unsigned int so I can compare it to n */
+	va_list list;
+	int sum = 0;
+	unsigned int i = 0;
+
 	if (n == 0)
 		return (0);
-	/* first condition of the task */
-	/* start iterating the arguments with type va_list */
-	/* first valist that hast type va_list followed by the first variable */
-	va_start(valist, n);
-
-	for (runner = 0; runner < n; runner++)
-	{
-		/* total is equal to the sum of the arguments */
-		/* starting with the type va_list (valist) and */
-		/* the type of the total number of arguments */
-		total += va_arg(valist, int);
-	}
-	/* free a the variable type va_list (valist) */
-	va_end(valist);
-	/* return the total of the sum and must be an int */
-	return (total);
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+	va_end(list);
+	return (sum);
 }
