87bed02221711335f20b1add48d961390e4511b4
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-19 21:10:37 +0300

    modified library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,17 +1,63 @@
 #ifndef CHRONICLE
 #define CHRONICLE
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
 
+/* STRUCTS */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for ALX project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int binary_search_helper(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
-/*int interpolation_search(int *array, size_t size, int value);*/
-/*int exponential_search(int *array, size_t size, int value);*/
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-/*listint_t *jump_list(listint_t *list, size_t size, int value);*/
-/*skiplist_t *linear_skip(skiplist_t *list, int value);*/
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* UTILITIES */
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 
 #endif
