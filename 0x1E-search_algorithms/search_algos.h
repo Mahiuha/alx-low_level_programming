850cbd7863ad6bcfd81b0554186dea07ad39057b
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-19 22:13:56 +0300

    modified library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,23 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -13,5 +30,10 @@ int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 /*listint_t *jump_list(listint_t *list, size_t size, int value);*/
 /*skiplist_t *linear_skip(skiplist_t *list, int value);*/
+
+/* UTILITIES */
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 
 #endif
