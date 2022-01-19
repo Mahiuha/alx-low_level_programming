4143639503510a0b3476380440d44bf0168b145a
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-19 21:16:30 +0300

    modified library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,10 +8,10 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_helper(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);*/
-int exponential_search(int *array, size_t size, int value);*/
+/*int interpolation_search(int *array, size_t size, int value);*/
+/*int exponential_search(int *array, size_t size, int value);*/
 int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);*/
-skiplist_t *linear_skip(skiplist_t *list, int value);*/
+/*listint_t *jump_list(listint_t *list, size_t size, int value);*/
+/*skiplist_t *linear_skip(skiplist_t *list, int value);*/
 
 #endif
